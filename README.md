5d309a1eb443131123f7f3beb6369243078cea1d
Xyc| Artem K. <zoom.2000v@gmail.com>
2026-02-15 20:04:34 +0200

    Add README and helloWorld.js example for JavaScript

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Artem-Kyrychenko-web2025-1
+
 this repo name could have been verbose-spoon
+
+anyway the only thing here is
+
+```{title:javascript}
+function helloWorld() {
+  console.log('Hello, World!');
+  console.log('And good morning to my creator Xoppyc/Xyc');
+}
+```
+
+with function calling for **node.js**
---
+++ helloworld.js
@@ -0,0 +1,6 @@
+function helloWorld() {
+  console.log('Hello, World!');
+  console.log('And good morning to my creator Xoppyc/Xyc');
+}
+helloWorld();
+//now this is a classic example of a hello world program in JavaScript. It defines a function called helloWorld that logs the string 'Hello, World!' to the console, and then calls that function to execute it. When you run this code, it will output 'Hello, World!' in the console. This is often used as a simple test to verify that a programming environment is set up correctly and that you can run basic code. Ussually, this is the first program that beginners write when learning a new programming language. Howeverr many programmers also use it as a quick test to ensure that their development environment is working properly or do funny things with it, like adding emojis or changing the message. It's a simple yet iconic piece of code that has become a tradition in programming culture.
